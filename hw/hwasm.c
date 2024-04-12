7ec3220a8f170fc4e66db4fcfe0309b408d0fe46
Tiago Oliva <tiago.oliva.costa@gmail.com>
2024-04-11 22:50:19 -0300

    start hwasm

---
+++ hw/hwasm.c
@@ -0,0 +1,13 @@
+/*
+ *    SPDX-FileCopyrightText: 2024 Luiz Antonio de Abreu Pereira
+ *    SPDX-FileCopyrightText: 2024 Tiago Oliva <tiago.oliva.costa@gmail.com>
+ *   
+ *    SPDX-License-Identifier: GPL-3.0-or-later
+ *
+ *    This file is part of SYSeg, available at https://gitlab.com/monaco/syseg.
+ */
+
+#include <string.h>
+
+int main(){
+}
