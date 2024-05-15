abdc257f76dd87a869eab150f649a7b05dfa63f8
Tiago Oliva <tiago.oliva.costa@gmail.com>
2024-05-14 21:27:20 -0300

    refactor: remove old unused project folder

--- syseg/eg/hw/build_run-eg-02_att.sh
+++
@@ -1,5 +0,0 @@
-as --32 eg-02_att.S -o eg-02_att.o
-
-ld -melf_i386 --oformat=binary -e begin -Ttext=0x7c00 eg-02_att.o -o eg-02_att.bin
-
-qemu-system-i386 -drive format=raw,file=eg-02_att.bin -net none
